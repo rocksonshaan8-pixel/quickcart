d52a94c9533f766a2333e84720e91f5370912557
Rockson Pinto <rocksonshaan8@gmail.com>
2026-03-13 11:21:42 +0530

    Part 2: Added shopping cart functionality

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,20 @@
 import "../styles/ProductCard.css";
 
-function ProductCard({ product }) {
+function ProductCard({ product, onAddToCart }) {
   return (
     <div className="card">
       <img src={product.image} alt={product.name} />
       <h3>{product.name}</h3>
       <p>{product.description}</p>
-      <p>{product.price}</p>
+      <p>${product.price}</p>
       <span>{product.category}</span>
+
+      <button
+        className="add-to-cart-btn"
+        onClick={() => onAddToCart(product)}
+      >
+        Add to Cart
+      </button>
     </div>
   );
 }
